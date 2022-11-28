a31d631038748400f270a1a0eef2910fc3283105
wss-git <wssgryx@163.com>
2022-11-28 09:35:13 +0800

    feat: npm publish

--- packages/npm-publish/src/index.ts
+++ packages/npm-publish/src/index.ts
@@ -1,13 +1,16 @@
-import { lodash, Logger, getInputs } from '@serverless-cd/core';
-const { get } = lodash;
+import { Logger, getInputs } from '@serverless-cd/core';
+import Publish, { IProps } from './publish';
+import os from 'os';
 
-export const run = (inputs: Record<string, any>, context: Record<string, any>, logger: Logger) => {
+export const run = async (inputs: Record<string, any>, context: Record<string, any>, logger: Logger) => {
   logger.info('start npm-publish');
   logger.info(`inputs: ${JSON.stringify(inputs)}`);
-  const newInputs = getInputs(inputs, context);
+  const newInputs = getInputs(inputs, context) as unknown as IProps;
   logger.info(`newInputs: ${JSON.stringify(newInputs)}`);
-  logger.info(`context: ${JSON.stringify(context)}`);
-  return { status: 'success', data: { name: get(inputs, 'name'), age: get(inputs, 'age') } };
+  logger.info(`newInputs: ${JSON.stringify(context)}`);
+  const publish = new Publish(newInputs, logger);
+  await publish.run(context.cwd || process.env.DOWNLOAD_CODE_DIR || os.tmpdir());
+  logger.info('end npm-publish');
 };
 
-// export default service;
+export default Publish;
